b98ca3534d0051c1ef9793c7272515e304f00308
Luca Dibattista <34377738+LucaDiba@users.noreply.github.com>
2020-04-14 18:03:19 +0200

    #18 Added multi-language support + [en, it, nl]

---
+++ src/plugins/translator/translator.js
@@ -0,0 +1,201 @@
+
+/**
+ * Translation credits
+ * 
+ * English: Luca Dibattista
+ * Italian: Luca Dibattista
+ * Dutch:   Gregory V. H.
+ */
+translator_strings = {
+    'languages': {
+        'en': 'English',
+        'it': 'Italiano',
+        'nl': 'Olandese'
+    },
+    'title': {
+        'en': 'Epidemic simulator',
+        'it': 'Simulatore di epidemie',
+        'nl': 'Epidemie simulator'
+    },
+    'infected': {
+        'en': 'infected',
+        'it': 'infetti',
+        'nl': 'geïnfecteerd'
+    },
+    'healthy': {
+        'en': 'healthy',
+        'it': 'sani',
+        'nl': 'gezond'
+    },
+    'cured': {
+        'en': 'cured',
+        'it': 'curati',
+        'nl': 'genezen'
+    },
+    'dead': {
+        'en': 'deaths',
+        'it': 'morti',
+        'nl': 'doden'
+    },
+    'Dead': {
+        'en': 'Dead',
+        'it': 'Morti',
+        'nl': 'Doden'
+    },
+    'of_which': {
+        'en': 'of which',
+        'it': 'di cui',
+        'nl': 'waarvan'  
+    },
+    'for_missing_beds': {
+        'en': 'for missing beds in intensive care',
+        'it': 'per mancanza di posti in terapia intensiva',
+        'nl': 'door niet genoeg plaatsen op intensive care'
+    },
+    'people': {
+        'en': 'people',
+        'it': 'persone',
+        'nl': 'mensen'
+    },
+    'quarantine': {
+        'en': 'Quarantine',
+        'it': 'Quarantena',
+        'nl': 'Quarantaine'
+    },
+    'intensive_care': {
+        'en': 'Intensive care',
+        'it': 'Terapia intensiva',
+        'nl': 'Intensive care'
+    },
+    'simulation_parameters': {
+        'en': 'Simulation parameters',
+        'it': 'Parametri simulazione',
+        'nl': 'Simulatie parameters'
+    },
+    'simulation_initial_population': {
+        'en': 'Initial population',
+        'it': 'Popolazione iniziale',
+        'nl': 'Initiële populatie'
+    },
+    'simulation_initial_population_help': {
+        'en': 'Number of total people',
+        'it': 'Persone totali',
+        'nl': 'Totaal aantal mensen'
+    },
+    'simulation_initial_infected': {
+        'en': 'Initial infected',
+        'it': 'Infetti iniziali',
+        'nl': 'Initieel aantal geïnfecteerd',
+    },
+    'simulation_initial_infected_help': {
+        'en': 'Number of initial people infected',
+        'it': 'Numero di persone inizialmente infette',
+        'nl': 'Aantal mensen die geïnfecteerd zijn',
+    },
+    'simulation_people_speed': {
+        'en': 'People speed',
+        'it': 'Velocità delle persone',
+        'nl': 'Snelheid van mensen',
+    },
+    'simulation_people_speed_help': {
+        'en': 'Speed with which people move.\nDecrease it to simulate lockdown',
+        'it': 'La velocità con la quale si muovono le persone.\nDiminuire per simulare il lockdown',
+        'nl': 'Snelheid waarmee mensen zich voortbewegen.\nVerminderen om lockdown te simuleren',
+    },
+    'simulation_desease_duration': {
+        'en': 'Desease duration',
+        'it': 'Durata malattia',
+        'nl': 'Ziektetijd',
+    },
+    'simulation_desease_duration_help': {
+        'en': 'How many days before dying or healing',
+        'it': 'Quanti giorni passano prima di guarire o morire',
+        'nl': 'Hoelang het duurt om te genezen',
+    },
+    'simulation_infection_rate': {
+        'en': 'Infection rate',
+        'it': 'Infettività',
+        'nl': 'Infectiegraad',
+    },
+    'simulation_infection_rate_help': {
+        'en': 'The probability for an infected to infect another person in the nearby',
+        'it': 'La probabilità per un infetto di infettare una persona che si trova nelle vicinanze',
+        'nl': 'De kans dat een geïnfecteerde persoon iemand anders infecteerd',
+    },
+    'simulation_lethality': {
+        'en': 'Lethality',
+        'it': 'Letalità',
+        'nl': 'Dodelijkheid',
+    },
+    'simulation_lethality_help': {
+        'en': 'The probability for an infected to die',
+        'it': 'La probabilità che un infetto muoia',
+        'nl': 'De kans dat iemand sterft aan het virus',
+    },
+    'simulation_activate_quarantine': {
+        'en': 'Activate quarantine',
+        'it': 'Attiva quarantena',
+        'nl': 'Activeer quarantaine',
+    },
+    'simulation_days_before_quarantine': {
+        'en': 'Days before quarantine',
+        'it': 'Giorni prima della quarantena',
+        'nl': 'Dagen vóór quarantaine',
+    },
+    'simulation_days_before_quarantine_help': {
+        'en': 'How many days before showing symphtoms and going in quarantine',
+        'it': 'Quanti giorni passano prima che si mostrino i sintomi e si vada in quarantena',
+        'nl': 'Hoeveel dagen voordat symptomen zichtbaar zijn en men in quarantaine gaat',
+    },
+    'simulation_asymptomatic_rate': {
+        'en': 'Asymptomatic rate',
+        'it': 'Asintomaticità',
+        'nl': 'Asymptomatiegraad',
+    },
+    'simulation_asymptomatic_rate_help': {
+        'en': 'How many, among people who get the virus, don\'t go in quarantine (maybe for not showing symphtoms)',
+        'it': 'Quanti, tra le persone che prendono in virus, non vanno in quarantena (magari per non aver mostrato i sintomi)',
+        'nl': 'Hoeveel mensen, onder de mensen die het virus hebben, niet in quarantaine gaan (mogelijks omdat geen symptomen zichtbaar zijn)',
+    },
+    'simulation_icu_rate': {
+        'en': 'Intensive care rate',
+        'it': 'Terapia intensiva',
+        'nl': 'Intensive care',
+    },
+    'simulation_icu_rate_help': {
+        'en': 'How many, among people who get the virus, need intensive care to not die',
+        'it': 'Quanti, tra le persone che prendono il virus, hanno bisogno della terapia intensiva per non morire',
+        'nl': 'Hoeveel mensen, onder de mensen die het virus hebben, moeten op intensive care verzorgd worden om te overleven',
+    },
+    'simulation_icu_every_100': {
+        'en': 'Number of IC units every 100 people',
+        'it': 'Posti in terapia intensiva ogni 100 persone',
+        'nl': 'Aantal IC bedden per 100 personen',
+    },
+    'simulation_icu_every_100_help': {
+        'en': 'E.g.: if there are 2 beds for 200 people, put "1" (1 bed every 100 people).\nIf there are no beds available, people who need IC will die',
+        'it': 'Esempio: se ci sono 2 posti per 200 persone, inserire "1" (1 letto ogni 100 persone).\nQuando i posti in terapia intensiva terminano, le persone che ne hanno bisogno moriranno',
+        'nl': 'Bijvoorbeeld: als er 2 bedden zijn voor 200 personen, zet dit op "1" (1 bed per 100 personen)\nAls er geen bedden beschikbaar zijn zullen mensen die deze zorgen nodig hebben sterven',
+    },
+    'stop_simulation': {
+        'en': 'Stop simulation',
+        'it': 'Ferma simulazione',
+        'nl': 'Stop simulatie',
+    },
+    'start_simulation': {
+        'en': 'Start simulation',
+        'it': 'Avvia simulazione',
+        'nl': 'Start simulatie',
+    },
+};
+
+function translateDocument(lang) {
+    document_strings = $('[data-translate]');
+
+    for(let i = 0; i < document_strings.length; i++) {
+        document_string = document_strings[i];
+        translation_id = document_string.dataset['translate'];
+        console.log(translation_id);
+        document_string.innerText = translator_strings[translation_id][lang];
+    }
+}
